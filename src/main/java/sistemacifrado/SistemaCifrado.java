fc21d98ef79aed9cadc069309859f10ac6d974c6
carloschinga <chinga_carlos@hotmail.com>
2025-09-10 16:24:29 -0500

    Implementacion Sistema de Cifrado

--- src/main/java/sistemacifrado/SistemaCifrado.java
+++ src/main/java/sistemacifrado/SistemaCifrado.java
@@ -1,13 +1,42 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package sistemacifrado;
 
-/**
- *
- * @author USER
- */
+
+
+
 public class SistemaCifrado {
+    public static String cifrar(String mensajeOriginal){
+        String mensajeCifrado="";        
+        int len= mensajeOriginal.length();        
+        for(int i=0;i<len;i++){
+            char c= mensajeOriginal.charAt(i);
+            int code=(int)c;
+            code+=1;
+            char nc=(char)code;
+            mensajeCifrado+=nc;            
+        }
+        return mensajeCifrado;
+    }
+    public static String descifrar(String mensajeDescifrado){
+        String mensajeCifrado="";        
+        int len= mensajeDescifrado.length();        
+        for(int i=0;i<len;i++){
+            char c= mensajeDescifrado.charAt(i);
+            int code=(int)c;
+            code-=1;
+            char nc=(char)code;
+            mensajeCifrado+=nc;            
+        }
+        return mensajeCifrado;
+    }
     
+    public static void main(String[] args) {
+        String mensajeOriginal="Srta. no debe llegar tarde";
+        String mensajeCifrado=SistemaCifrado.cifrar(mensajeOriginal);
+        String mensajeDescifrado=SistemaCifrado.descifrar(mensajeCifrado);
+        
+        System.out.println(mensajeOriginal);
+        System.out.println(mensajeCifrado);
+        System.out.println(mensajeDescifrado);
+        
+    }
 }
